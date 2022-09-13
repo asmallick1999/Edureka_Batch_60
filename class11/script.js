b36966f4ebed89aee4b48fe096b838b64c944c76
asmallick1999 <asmallick1999@gmail.com>
2022-09-13 16:48:13 +0530

    add class11 folder and added class 11 practice js

---
+++ class11/script.js
@@ -0,0 +1,34 @@
+// 1
+
+// const arr = [10,16,50,7,44,70]
+
+// console.log(arr.filter(above18=>above18>=18))
+
+// console.log(arr.find(above18))
+
+
+
+
+
+
+// 2
+
+class Student{
+    name;
+    rollNo;
+    age;
+    class;
+    exam(){
+        return "On December"
+    }
+    perticipateCompetetion(){
+
+    }
+}
+const std1 = new Student();
+std1.name="sohail";
+std1.rollNo=203003;
+std1.age=23;
+std1.class="4th Year"
+std1.exam()
+console.log(std1);
